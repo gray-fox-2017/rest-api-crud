220a3ce5e2cb9dd618f3500a59732e9f143340d8
hakiemaul <noersedya@gmail.com>
2017-05-22 21:00:02 +0700

    add admin for heroku

--- tes.js
+++ tes.js
@@ -1,6 +1,6 @@
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 'budi123';
+const myPlaintextPassword = 'john123';
 const someOtherPlaintextPassword = 'not_bacon';
 
 bcrypt.genSalt(saltRounds, function(err, salt) {
@@ -9,9 +9,9 @@ bcrypt.genSalt(saltRounds, function(err, salt) {
     });
 });
 
-let hash = "$2a$10$3ALoXdnJYbyVIEHtT5mwa.5i/EAbKLk8rHTUX5pKzCyS4r42vmuet";
-
-bcrypt.compare("budi123", "$2a$10$fK/W8CkOkAQrQzSZVf2FlO03EeNpq2wgjOmUf5Ef0DmOz9p1P1pXG", function(err, res) {
-    // res == true
-    console.log(res)
-});
+// let hash = "$2a$10$3ALoXdnJYbyVIEHtT5mwa.5i/EAbKLk8rHTUX5pKzCyS4r42vmuet";
+//
+// bcrypt.compare("budi123", "$2a$10$fK/W8CkOkAQrQzSZVf2FlO03EeNpq2wgjOmUf5Ef0DmOz9p1P1pXG", function(err, res) {
+//     // res == true
+//     console.log(res)
+// });
