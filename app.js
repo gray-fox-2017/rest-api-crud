c65a73e95713ece8e1606094eef88a2537ba2c02
aldyadk <aldy.andika@gmail.com>
2017-05-22 17:16:57 +0700

    coba solving masalah upload ke heroku 2

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const api = require('./routes/api');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/',(req,res)=>{
-  res.send('asd')
-})
 app.use('/api',api)
 
 app.listen(process.env.PORT||3000,()=>{
