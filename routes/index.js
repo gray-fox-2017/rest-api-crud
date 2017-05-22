07ca94eec9ac7da42230330e5f0f893465fc5858
raynormw <tirtawiryaputra@yahoo.com>
2017-05-22 22:32:45 +0700

    welcome message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('https://github.com/raynormw/rest-api-crud for usage');
+  res.send('Welcome to Hacktiv8 database! go to https://github.com/raynormw/rest-api-crud for usage');
 });
 
 module.exports = router;
