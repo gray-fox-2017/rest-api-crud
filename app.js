70fc73a96a3496069bed98b9b21506816703215c
aldyadk <aldy.andika@gmail.com>
2017-05-22 17:09:30 +0700

    coba benerin buat ke heroku

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const api = require('./routes/api');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/',(req,res)=>{
+  res.send('asd')
+})
 app.use('/api',api)
 
 app.listen(3000,()=>{
