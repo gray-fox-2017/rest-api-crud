dd1596b769d5206d396de298c5de9ccb7707cae1
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-22 20:43:25 +0700

    udaptes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var users_controller = require('../controllers/users_controller.js')
 
 /* GET home page. */
+router.get('/', function(req, res) {
+  res.send('https://github.com/erwinwahyura/rest-api-crud')
+})
+
 router.get('/api/users', users_controller.getUser);
 router.get('/api/users/:id', users_controller.getOneUser );
 router.post('/api/users', users_controller.addUser );
