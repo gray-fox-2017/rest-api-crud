ea87c7a19af799a5d92c3daaf898537efdb5ef27
aldyadk <aldy.andika@gmail.com>
2017-05-22 17:43:12 +0700

    done revisi update

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const api = require('./routes/api');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/api',api)
 app.use('/',(req,res)=>{
   res.redirect('https://github.com/aldyadk/rest-api-crud')
 })
-app.use('/api',api)
 
 app.listen(process.env.PORT||3000,()=>{
   console.log('udah jalan cuy');
