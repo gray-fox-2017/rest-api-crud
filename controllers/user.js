4c57aac42080fc0ee52efeea8d967621132d5f2a
parelhutahaean <parel.hutahaean@gmail.com>
2017-05-22 17:02:28 +0700

    Adding authorization

--- controllers/user.js
+++ controllers/user.js
@@ -1,49 +1,125 @@
 var models = require('../models')
+var bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken')
 
 var methods = {}
 
-methods.create = (req, res) => {
-  models.User.create(req.body)
-  .then(user => {
-    res.send(user)
+methods.signup = (req, res) => {
+  var saltRounds = 10
+  bcrypt.hash(req.body.password, saltRounds)
+  .then(hash => {
+    models.User.create({
+      username: req.body.username,
+      password: hash,
+      role: 'admin'
+    })
+    .then(user => {
+      res.send(user)
+    })
   })
+}
+
+methods.signin = (req, res) => {
+  models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  .then(user => {
+    bcrypt.compare(req.body.password, user.password)
+    .then(decoded => {
+      var token = jwt.sign({
+        username: user.username,
+        role: user.role,
+        id: user.id
+      }, 'rahasia', { expiresIn: '3h' });
+      if (token) {
+        res.send({token})
+      }
+    })
+  })
+}
+
+methods.create = (req, res) => {
+  if (req.user.role == 'admin') {
+    var saltRounds = 10
+    bcrypt.hash(req.body.password, saltRounds)
+    .then(hash => {
+      models.User.create({
+        username: req.body.username,
+        password: hash,
+        role: req.body.role
+      })
+      .then(user => {
+        res.send(user)
+      })
+    })
+  } else {
+    res.send({msg: 'User not authorized'})
+  }
 }
 
 methods.readAll = (req, res) => {
-  models.User.findAll()
-  .then(users => {
-    res.send(users)
-  })
+  if (req.user.role == 'admin') {
+    models.User.findAll({
+      order: [
+        ['id', 'ASC']
+      ]
+    })
+    .then(users => {
+      res.send(users)
+    })
+  } else {
+    res.send({msg: 'User not authorized'})
+  }
 }
 
 methods.read = (req, res) => {
-  models.User.findById(req.params.id)
-  .then(user => {
-    res.send(user)
-  })
+  if (req.user.id == req.params.id || req.user.role == 'admin') {
+    models.User.findById(req.params.id)
+    .then(user => {
+      res.send(user)
+    })
+  } else {
+    res.send({msg: 'User not authorized'})
+  }
+
 }
 
 methods.update = (req, res) => {
-  models.User.findById(req.params.id)
-  .then(user => {
-    user.update({
-      username: req.body.username || user.username,
-      password: req.body.password || user.password
+  if (req.user.role == 'admin') {
+    models.User.findById(req.params.id)
+    .then(user => {
+      var saltRounds = 10
+      bcrypt.hash(req.body.password, saltRounds)
+      .then(function(hash) {
+        user.update({
+          username: req.body.username || user.username,
+          password: hash || user.password,
+          role: req.body.role || user.role
+        })
+        .then(updated => {
+          res.send(updated)
+        })
+      })
     })
-    .then(updated => {
-      res.send(updated)
-    })
-  })
+  } else {
+    res.send({msg: 'User not authorized'})
+  }
 }
 
 methods.delete = (req, res) => {
-  models.User.findById(req.params.id)
-  .then(user => {
-    user.destroy()
-    .then((yes) => {
-      res.send({msg: `User ${user.username} has been deleted`})
+  if (req.user.role == 'admin') {
+    models.User.findById(req.params.id)
+    .then(user => {
+      user.destroy()
+      .then((yes) => {
+        res.send({msg: `User ${user.username} has been deleted`})
+      })
     })
-  })
+  } else {
+    res.send({msg: 'User not authorized'})
+  }
 }
 
 module.exports = methods
--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,24 @@
 var router = require('express').Router()
 var models = require('../models')
 var userController = require('../controllers/user')
+var utils = require('../helpers/utils')
 
 router.get('/', (req, res) => {
   res.send('Alive from router')
 })
 
-router.get('/users', userController.readAll)
+router.post('/signup', userController.signup)
 
-router.get('/users/:id', userController.read)
+router.post('/signin', userController.signin)
 
-router.post('/users', userController.create)
+router.get('/users', utils.isValidUser, userController.readAll)
 
-router.put('/users/:id', userController.update)
+router.get('/users/:id', utils.isValidUser, userController.read)
 
-router.delete('/users/:id', userController.delete)
+router.post('/users', utils.isValidUser, userController.create)
+
+router.put('/users/:id', utils.isValidUser, userController.update)
+
+router.delete('/users/:id', utils.isValidUser, userController.delete)
 
 module.exports = router
